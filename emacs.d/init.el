c5e8505a9fd1ed50668884439d4339f282c1bce1
Petter Wigle <petter.wigle@magine.com>
2013-03-25 09:10:31 +0100

    Settings for Javascript

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -198,3 +198,17 @@
 (add-to-list 'auto-mode-alist '("\\.md" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.avsc" . json-mode))
 (add-to-list 'auto-mode-alist '("\\.avpr" . json-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(js-indent-level 2)
+ '(js2-basic-offset 2))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
