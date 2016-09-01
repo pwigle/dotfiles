488d06ef7824f7ee9e0ff87d464aff942f4270dd
Petter Wigle <petter.wigle@magine.com>
2016-09-01 14:54:09 +0200

    Rebooted init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,22 +1,38 @@
 (require 'package)
 (add-to-list 'package-archives
-	     '("melpa-stable" . "http://stable.melpa.org/packages/") t)
-(add-to-list 'package-archives
-	     '("melpa.org" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives
-	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
+	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+;;; Packages
+(setq packages
+      '(ensime
+	nyan-mode
+	solarized-theme))
 
 (package-initialize)
 
-(when (not package-archive-contents)
-  (package-refresh-contents))
+(dolist (pkg packages)
+  (unless (package-installed-p pkg)
+    (package-refresh-contents) (package-install pkg)))
 
-;; packages
-(defvar my-packages '(starter-kit
-		      starter-kit-lisp
-		      starter-kit-bindings)
-  "A list of packages to ensure are installed at launch.")
 
-(dolist (p my-packages)
-  (when (not (package-installed-p p))
-    (package-install p)))
+(require 'ido)
+(ido-mode t)
+
+;; Scala
+(require 'ensime)
+
+(setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
+(setq exec-path (append exec-path '("/usr/local/bin")))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(indent-tabs-mode nil)
+ '(initial-frame-alist (quote ((fullscreen . maximized))))
+ '(js-indent-level 2))
+
+(tool-bar-mode -1)
+(blink-cursor-mode 0)
+
