52115f26f3590b4353c02881314a15f13fe1bd48
Petter Wigle <petter.wigle@factor10.com>
2018-11-02 09:35:21 +0100

    Add emacs packages

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -3,17 +3,21 @@
 
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+             '("melpa-stable" . "https://melpa.org/packages/") t)
 
 ;;; Packages
 (setq packages
       '(ensime
+	restclient
 	nyan-mode
         yaml-mode
         clojure-mode
         json-mode
         markdown-mode
         elm-mode
+        pug-mode
+        coffee-mode
+        typescript-mode
 	solarized-theme))
 
 (package-initialize)
@@ -33,15 +37,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
  '(indent-tabs-mode nil)
  '(initial-frame-alist (quote ((fullscreen . maximized))))
  '(js-indent-level 2)
  '(package-selected-packages
    (quote
-    (elm-mode solarized-theme markdown-mode json-mode clojure-mode yaml-mode nyan-mode ensime))))
+    (typescript-mode elm-mode solarized-theme markdown-mode json-mode clojure-mode yaml-mode nyan-mode ensime))))
 (tool-bar-mode -1)
 (blink-cursor-mode 0)
 (setq make-backup-files nil)
+(set-default-font "Monaco 14")
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
