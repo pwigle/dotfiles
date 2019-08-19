193648fec55fe0a87cac402de4331520abac6b14
Petter Wigle <petter.wigle@apoex.se>
2019-08-19 07:57:36 +0200

    Add some emacs modes

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -11,6 +11,7 @@
 	restclient
 	nyan-mode
         yaml-mode
+        haml-mode
         clojure-mode
         json-mode
         markdown-mode
@@ -18,6 +19,10 @@
         pug-mode
         coffee-mode
         typescript-mode
+        elixir-mode
+        alchemist
+        dumb-jump
+        terraform-mode
 	solarized-theme))
 
 (package-initialize)
@@ -37,6 +42,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(coffee-tab-width 2)
  '(custom-safe-themes
    (quote
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
@@ -45,11 +51,14 @@
  '(js-indent-level 2)
  '(package-selected-packages
    (quote
-    (typescript-mode elm-mode solarized-theme markdown-mode json-mode clojure-mode yaml-mode nyan-mode ensime))))
+    (terraform-mode alchemist elixir-mode haml-mode dumb-jump typescript-mode elm-mode solarized-theme markdown-mode json-mode clojure-mode yaml-mode nyan-mode ensime))))
 (tool-bar-mode -1)
 (blink-cursor-mode 0)
 (setq make-backup-files nil)
 (set-default-font "Monaco 14")
+(setq ruby-insert-encoding-magic-comment nil)
+
+(dumb-jump-mode)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
