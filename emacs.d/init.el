328ae82d8ea029d5d3f02c7361bdc05b43133222
Petter Wigle <petter.wigle@apoex.se>
2019-10-08 15:19:30 +0200

    Set tango-dark theme in Emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,3 +1,6 @@
+(add-to-list 'load-path "~/.emacs.d/vendor")
+(require 'rubocopfmt)
+
 (setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
 (setq exec-path (append exec-path '("/usr/local/bin")))
 
@@ -35,33 +38,25 @@
 (require 'ensime)
 (ido-mode t)
 
-;; Scala
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(coffee-tab-width 2)
- '(custom-safe-themes
-   (quote
-    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
- '(indent-tabs-mode nil)
- '(initial-frame-alist (quote ((fullscreen . maximized))))
- '(js-indent-level 2)
- '(package-selected-packages
-   (quote
-    (terraform-mode alchemist elixir-mode haml-mode dumb-jump typescript-mode elm-mode solarized-theme markdown-mode json-mode clojure-mode yaml-mode nyan-mode ensime))))
 (tool-bar-mode -1)
 (blink-cursor-mode 0)
 (setq make-backup-files nil)
 (set-default-font "Monaco 14")
 (setq ruby-insert-encoding-magic-comment nil)
-
 (dumb-jump-mode)
+(setq initial-frame-alist (quote ((fullscreen . maximized))))
+(load-theme 'tango-dark)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))))
